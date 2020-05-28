f05cd7ad5f8ddf916ecc163708e450962518df3d
ellakathir <ellakathir75@gmail.com>
2020-05-28 22:06:01 +0530

    Add files via upload

---
+++ day 3.java..java
@@ -0,0 +1,181 @@
+1)find the integer array and find the maximum and minimum.
+import java.util.Arrays; 
+public class Exercise10 {
+ 
+  static int max;
+  static int min;
+
+    public static void max_min(int my_array[]) {
+        max = my_array[0];
+        min = my_array[0];
+        int len = my_array.length;
+        for (int i = 1; i < len - 1; i = i + 2) {
+            if (i + 1 > len) {
+                if (my_array[i] > max) max = my_array[i];
+                if (my_array[i] < min) min = my_array[i];
+            }
+            if (my_array[i] > my_array[i + 1]) {
+                if (my_array[i] > max) max = my_array[i];
+                if (my_array[i + 1] < min) min = my_array[i + 1];
+            }
+            if (my_array[i] < my_array[i + 1]) {
+                if (my_array[i] < min) min = my_array[i];
+                if (my_array[i + 1] > max) max = my_array[i + 1];
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+           int[] my_array = {25, 14, 56, 15, 36, 56, 77, 18, 29, 49};
+        max_min(my_array);
+        System.out.println(" Original Array: "+Arrays.toString(my_array));
+        System.out.println(" Maximum value for the above array = " + max);
+        System.out.println(" Minimum value for the above array = " + min);
+    }
+}
+2)program to initialize and print them in a sorted order
+public class SortAsc {    
+    public static void main(String[] args) {        
+            
+            
+        int [] arr = new int [] {5, 2, 8, 7, 1};     
+        int temp = 0;    
+            
+          
+        System.out.println("Elements of original array: ");    
+        for (int i = 0; i < arr.length; i++) {     
+            System.out.print(arr[i] + " ");    
+        }    
+            
+          
+        for (int i = 0; i < arr.length; i++) {     
+            for (int j = i+1; j < arr.length; j++) {     
+               if(arr[i] > arr[j]) {    
+                   temp = arr[i];    
+                   arr[i] = arr[j];    
+                   arr[j] = temp;    
+               }     
+            }     
+        }    
+          
+        System.out.println();    
+            
+3)program to remove the duplicate elements and print the same 
+public class RemoveDuplicateInArrayExample{  
+public static int removeDuplicateElements(int arr[], int n){  
+        if (n==0 || n==1){  
+            return n;  
+        }  
+        int[] temp = new int[n];  
+        int j = 0;  
+        for (int i=0; i<n-1; i++){  
+            if (arr[i] != arr[i+1]){  
+                temp[j++] = arr[i];  
+            }  
+         }  
+        temp[j++] = arr[n-1];     
+        
+        for (int i=0; i<j; i++){  
+            arr[i] = temp[i];  
+        }  
+        return j;  
+    }  
+       
+    public static void main (String[] args) {  
+        int arr[] = {10,20,20,30,30,40,50,50};  
+        int length = arr.length;  
+        length = removeDuplicateElements(arr, length);  
+        
+        for (int i=0; i<length; i++)  
+           System.out.print(arr[i]+" ");  
+    }  
+}  
+            
+        System.out.println("Elements of array sorted in ascending order: ");    
+        for (int i = 0; i < arr.length; i++) {     
+            System.out.print(arr[i] + " ");    
+        }    
+    }    
+}    
+
+4)program to initialize and print the sum and average
+import java.util.Scanner;
+public class Sum_Average
+{
+    public static void main(String[] args) 
+    {
+        int n, sum = 0;
+        float average;
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter no. of elements you want in array:");
+        n = s.nextInt();
+        int a[] = new int[n];
+        System.out.println("Enter all the elements:");
+        for(int i = 0; i < n ; i++)
+        {
+            a[i] = s.nextInt();
+            sum = sum + a[i];
+        }
+        System.out.println("Sum:"+sum);
+        average = (float)sum / n;
+        System.out.println("Average:"+average);
+    }
+}
+5)initialize an integer array with ascii values and print
+public class AsciiValue { 
+
+	public static void main(String[] args) 
+	{ 
+
+		char c = 'e'; 
+		int ascii = c; 
+		System.out.println("The ASCII value of " + c + " is: " + ascii); 
+	} 
+} 
+6)to find largest 2 numbers and smallest 2 numbers
+import java.util.Scanner;
+public class largest_and_second
+{
+	public static void main (String[] args)
+	{
+		Scanner scn = new Scanner (System.in);
+		System.out.print("Enter no. of elements you want in array:");
+                int n = scn.nextInt();
+ 
+		int array[] = new int[n];
+                System.out.println("Enter all the elements:");
+		for (int i = 0; i < array.length; i++)
+		{
+			array[i] = scn.nextInt();
+		}
+ 
+		int largest1, largest2, temp;
+ 
+		largest1 = array[0];
+		largest2 = array[1];
+ 
+		if (largest1 < largest2)
+		{
+			temp = largest1;
+			largest1 = largest2;
+			largest2 = temp;
+		}
+ 
+		for (int i = 2; i < array.length; i++)
+		{
+			if (array[i] > largest1)
+			{
+				largest2 = largest1;
+				largest1 = array[i];
+			}
+			else if (array[i] > largest2 && array[i] != largest1)
+			{
+				largest2 = array[i];
+			}
+		}
+ 
+		System.out.println ("The First largest is " + largest1);
+		System.out.println ("The Second largest is " + largest2);
+ 
+	}
+}
